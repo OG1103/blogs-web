97001fa8d301c2a48999288a6bc838f04303358b
Omar Gaber <omargaber1103@gmail.com>
2025-03-16 22:52:55 +0200

    Remove useclient

--- src/app/Components/Auth/LoginForm.tsx
+++ src/app/Components/Auth/LoginForm.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type React from "react";
 import { useState } from "react";
 import { TextField, Button, Box, CircularProgress } from "@mui/material";
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useRouter } from "next/router";
 import MainLayout from "@/app/Layouts/basicLayout";
 import PostDetail from "@/app/Components/Posts/PostDetail";
